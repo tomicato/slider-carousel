c16b930633293d6efb5af2f640f180c7184e9dc7
tomicato <sfinks134@gmail.com>
2019-07-09 00:01:50 +0700

    native js slider oop

--- js/slider.js
+++ js/slider.js
@@ -5,17 +5,18 @@ $(function () {
     prev: '#gallery .buttons .prev',
     next: '#gallery .buttons .next',
     auto: false,
-
+    rate: 2000
   });
 
-/*  new slideObj({
-    images: '#gallery .photos img',
-    prev: '#gallery .buttons .prev',
-    next: '#gallery .buttons .next',
-    auto: true,
-    rate: 2000
-  });*/
+  new slideObj2({
+    images: '#gallery .photos2 img',
+    prev: '#gallery .buttons2 .prev2',
+    next: '#gallery .buttons2 .next2',
+    auto: false,
+    rate: 5000
+  });
 
+/*=====================First Slider===========================*/
 
   function slideObj(obj) {
     this.images = $(obj.images);
@@ -27,9 +28,6 @@ $(function () {
     var i = 0;
     var ctx = this;
 
-    ctxWidth = ctx.images.eq(0).width();
-    ctxHeight = ctx.images.eq(0).height();
-
     var isRun = false;
 
 
@@ -40,7 +38,7 @@ $(function () {
       }
 
       isRun = true;
-
+    // Скрыть предыдущий
       ctx.images.eq(i).css({
         left: 0,
         top: 0,
@@ -53,7 +51,7 @@ $(function () {
       if(i < 0){
         i = ctx.images.length - 1;
       }
-
+      //Показать предыдущий
       ctx.images.eq(i).css({
         left: 'auto',
         top: 'auto',
@@ -67,7 +65,7 @@ $(function () {
     };
 
 
-    // Показать следующий
+    // Скрыть следующий
     this.nextSlide = function () {
 
       if(isRun){
@@ -86,7 +84,7 @@ $(function () {
       }, 2000);
       i++;
 
-
+    // Показать следующий
       if(i >= ctx.images.length){
         i = 0;
       }
@@ -116,5 +114,87 @@ $(function () {
     }
 
   }
+
+
+  /*=====================Second Slider===========================*/
+
+  function slideObj2(obj) {
+
+    this.next = obj.next;
+    this.prev = obj.prev;
+    this.img = $(obj.images);
+    this.auto = obj.auto;
+    this.rate = obj.rate || 5000;
+
+    var j = 0;
+    var slider = this;
+
+    sWt= slider.img.eq(0).width();
+    sHt = slider.img.eq(0).height();
+
+
+    // Предыдущий слайд
+    this.prevSlide2 = function () {
+
+    // Скрыть предыдущий слайд
+      slider.img.eq(j).css({
+        height: sHt,
+        width: sWt,
+      }).animate({
+        left: - sWt,
+      }, 2000);
+      j--;
+      if(j < 0){
+        j = slider.img.length - 1;
+      }
+
+      // Показать предыдущий слайд
+      slider.img.eq(j).css({
+        height: sHt,
+        width: sWt,
+        left: sWt,
+      }).animate({
+        left:0,
+      }, 2000);
+
+    };
+
+
+    // Следующий слайд
+    this.nextSlide2 = function () {
+
+    // Скрыть следующий слайд
+      slider.img.eq(j).css({
+        height: sHt,
+        width: sWt,
+      }).animate({
+        left:  sWt,
+      }, 2000);
+      j++;
+      if(j >= slider.img.length){
+        j =  0;
+      }
+
+      // Показать следующий слайд
+      slider.img.eq(j).css({
+        height: sHt,
+        width: sWt,
+        left: -sWt,
+      }).animate({
+        left:0,
+      }, 2000);
+
+    };
+
+    if(slider.auto){
+      setInterval(slider.nextSlide2, slider.rate);
+    }
+
+
+    $(slider.prev).on('click', function () {slider.prevSlide2();});
+    $(slider.next).on('click', function () {slider.nextSlide2();});
+
+  }
+
 });
 
